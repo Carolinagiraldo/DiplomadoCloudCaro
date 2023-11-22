8869922c1186196c646c42ddab87aa34a1e5b2d5
EC2 Default User <ec2-user@ip-172-31-12-168.ec2.internal>
2023-11-22 01:05:00 +0000

    secrea para subir imagen con ia

--- Diplomado.py
+++ Diplomado.py
@@ -45,7 +45,7 @@ def insertar_elemento_tabla(nombre_tabla, id, nombre, apellido):
         }
     )
     print("Elemento insertado exitosamente")
-    
+ #opcion 4   
 #leer los elementos de una tabla de dynamodb
 def leer_elementos_tabla(nombre_tabla):
     dynamodb = boto3.resource('dynamodb')
@@ -54,6 +54,26 @@ def leer_elementos_tabla(nombre_tabla):
     items = response['Items']
     for item in items:
         print(item)
+
+#eliminar elementos de una tabla de dynamondb
+def eliminar_elemento_tabla(nombre_tabla, id):
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
+    table.delete_item(
+        Key={
+            'id': id
+        }
+    )
+    print("Elemento eliminado exitosamente")
+    
+#reconocer celebridad en imagen
+def reconocer_celebridad_imagen(imagen):
+    rekognition = boto3.client('rekognition')
+    response = rekognition.recognize_celebrities(Image={'Bytes': imagen})
+    for celebrity in response['CelebrityFaces']:
+        print(response['celebrityFaces'][0]['Name'])
+        
         
 opcion = 1 
 
@@ -63,6 +83,8 @@ while opcion != 0:
     print("2. crear nueva tabla")
     print("3. insertar elemento en tabla")
     print("4. leer elementos de tabla")
+    print("5. eliminar elemento de tabla")
+    print("6. reconocer celebridad en imagen")
     print("0. salir")
     opcion = int(input())
     
@@ -79,7 +101,16 @@ while opcion != 0:
         insertar_elemento_tabla(nombre_tabla, id, nombre, apellido)  
     elif opcion == 4:
         nombre_tabla = input("ingrese el nombre ")
-        leer_elementos_tabla(nombre_tabla)    
+        leer_elementos_tabla(nombre_tabla)   
+    elif opcion == 5:
+        nombre_tabla = input("ingrese el nombre de la tabla")
+        id = input("ingrese el id del elementos ")
+        eliminar_elemento_tabla(nombre_tabla, id)   
+    elif opcion == 6:
+       #convertir imagen a bytes
+        with open('imagen.jpg', 'rb') as f:     #para insertar la imagen solo en imagen.jpg pongo el nombre de la mia.
+            imagen = f.read()
+            reconocer_celebridad_imagen(imagen)
     elif opcion == 0:
         print("hasta luego")
     else:
