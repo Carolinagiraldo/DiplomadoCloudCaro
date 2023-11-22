9494dc5ddc1160c80010a60c99dabcd72c91ca98
EC2 Default User <ec2-user@ip-172-31-12-168.ec2.internal>
2023-11-22 00:24:39 +0000

    se agregan elementos para insertar elementos de una tabla opcion 2 3 y 4

--- Diplomado.py
+++ Diplomado.py
@@ -31,6 +31,29 @@ def crear_tabla_dynamodb(nombre_tabla):
     )
     print("Tabla creada exitosamente")
      
+    
+   #opcion3      
+#crear funcion para insertar elemento en tabla de dynamodb
+def insertar_elemento_tabla(nombre_tabla, id, nombre, apellido):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
+    table.put_item(
+        Item={
+            'id': id,
+            'nombre': nombre,
+            'apellido': apellido
+        }
+    )
+    print("Elemento insertado exitosamente")
+    
+#leer los elementos de una tabla de dynamodb
+def leer_elementos_tabla(nombre_tabla):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
+    response = table.scan()
+    items = response['Items']
+    for item in items:
+        print(item)
         
 opcion = 1 
 
@@ -38,6 +61,8 @@ while opcion != 0:
     print("ingrese la opcion que desea")
     print("1. listar tabla de dynamobd de la cuenta")
     print("2. crear nueva tabla")
+    print("3. insertar elemento en tabla")
+    print("4. leer elementos de tabla")
     print("0. salir")
     opcion = int(input())
     
@@ -46,6 +71,15 @@ while opcion != 0:
     elif opcion == 2:
         nombre_tabla = input("Ingrese el nombre de la tabla: ")
         crear_tabla_dynamodb(nombre_tabla)
+    elif opcion == 3:
+        nombre_tabla = input("Ingrese el nombre de la tabla: ")
+        id = input("Ingrese el id: ")
+        nombre = input("Ingrese el nombre: ")
+        apellido = input("Ingrese el apellido: ")
+        insertar_elemento_tabla(nombre_tabla, id, nombre, apellido)  
+    elif opcion == 4:
+        nombre_tabla = input("ingrese el nombre ")
+        leer_elementos_tabla(nombre_tabla)    
     elif opcion == 0:
         print("hasta luego")
     else:
