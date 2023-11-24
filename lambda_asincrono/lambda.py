1f73e9d95244d7b0da5050fa5a6315687ef012cb
EC2 Default User <ec2-user@ip-172-31-12-168.ec2.internal>
2023-11-24 21:32:04 +0000

    se actualiza lambda para obtener nombre de bucket y nombre

---
+++ lambda_asincrono/lambda.py
@@ -0,0 +1,22 @@
+#read csv file
+import csv
+import json
+
+def readcsv(file):
+ with open(file) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
+        
+def lambda_handler(event, context):
+    
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    objeto = event['Records'][0]['s3']['objeto']['key']
+    
+    print(bucket)
+    print(objeto)
+   
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
